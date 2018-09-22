86df7d00215827f9d79213728273036265e5a064
dhdc1 <dhdctraining@gmail.com>
2018-09-22 18:00:01 +0700

    auto

--- modules/queuemanage/controllers/DefaultController.php
+++ modules/queuemanage/controllers/DefaultController.php
@@ -16,8 +16,16 @@ class DefaultController extends Controller
      */
     public function actionIndex()
     {
-        $sql = "select 1";
+        $sql = "SELECT t.pcc_vn,p.hn,t.visit_date_begin,t.visit_time_begin 
+,concat(p.prename,p.fname,' ',p.lname) fullname
+from pcc_visit t 
+LEFT JOIN pcc_patient  p ON p.cid = t.person_cid
+WHERE t.visit_date_begin BETWEEN '2018-09-22' AND '2018-09-22'
+AND t.current_station = 'A0' order by t.visit_date_begin asc,t.visit_time_begin asc";
+        
         $raw = DbHelper::queryAll('db', $sql);
-        return $this->render('index');
+        return $this->render('index',[
+            'raw'=>$raw
+        ]);
     }
 }
--- modules/queuemanage/views/default/index.php
+++ modules/queuemanage/views/default/index.php
@@ -1,6 +1,8 @@
 <?php
 
 use app\components\MessageHelper;
+use app\assets\DataTableAsset;
+DataTableAsset::register($this);
 ?>
 <div class="panel panel-info">
 
@@ -15,7 +17,7 @@ use app\components\MessageHelper;
         <?= MessageHelper::Note(" แสดง Lab detailview ขวามือ เมื่อคลิก/hover ที่ คนไข้ ") ?>
         <?= MessageHelper::Note(" เลือกห้องตรวจก่อนส่งเข้าตรวจทีละหลายคน ") ?>
         <?= MessageHelper::Note(" คลิกแล้วเรียกลำดับ ตัวเลขลำดับส่งเข้าตรวจเปลี่ยนตามคลิก ก่อน/หลัง ") ?>
-<?= MessageHelper::Note("ลง SCREEN /VST / CC /PE /PI ก่อนเข้าพบแพทย์ที่นี่??? (กรณีไม่ลงที่ JHCIS)") ?>
+        <?= MessageHelper::Note("ลง SCREEN /VST / CC /PE /PI ก่อนเข้าพบแพทย์ที่นี่??? (กรณีไม่ลงที่ JHCIS)") ?>
         <div style="margin-bottom: 3px">
             <button class="btn btn-info" onClick=swal("ยืนยันส่งคนที่เลือก...")><i class="fa fa-check"></i> ส่งพบแพทย์</button>
             <a class="btn btn-danger pull-right" href="#"><i class="fa fa-user-md" aria-hidden="true"></i> ตั้งค่า</a>        
@@ -27,41 +29,30 @@ use app\components\MessageHelper;
                     <table class="table table-striped table-bordered">
                         <thead>
                             <tr>
-                                <th></th>
-                                <th>#</th>
+                                <th></th>                                
                                 <th>Hn</th>
-                                <th>เวลามา</th>
-                                <th>เวลานัด</th>
+                                <th></th>
+                                <th>เวลามา</th>                                
                                 <th>ชื่อ นามสกุล</th>
                                 <th>รายการตรวจสอบ</th>
                             </tr>
                         </thead>
                         <tbody>
 
-                            <tr data-key="0">
-                                <td>
-                                    <input type="checkbox" name="selection[]" value="0" />
-                                </td>
-                                <td>1</td>
-                                <td>000000009</td>
-                                <td>20/09/2561 เวลา 14:09:14</td>
-                                <td>-</td>
-                                <td>น.ส.สวยงาม สอง มากเลย</td>
-                                <td><span style='color:green'>Lab</span></td>
-                            </tr>
-                            <tr data-key="1">
-                                <td><input type="checkbox" name="selection[]" value="1"/></td>
-                                <td>2</td><td>000000012</td>
-                                <td>20/09/2561 เวลา 14:09:59</td>
-                                <td>-</td><td>นายพ็อกบ้า  ยิ่งมั่ว</td>
-                                <td><span style='color:green'>Lab</span></td>
-                            </tr>
-                            <tr data-key="2"><td><input type="checkbox" name="selection[]" value="2" /></td>
-                                <td>3</td><td>000000004</td>
-                                <td>20/09/2561 เวลา 14:11:02</td>
-                                <td>-</td><td>นายสมชาย  มีมาก</td>
-                                <td><span style='color:green'>Lab</span></td>
-                            </tr>
+                            <?php foreach ($raw as $key => $value): ?>
+                                <tr data-key="<?= $value['pcc_vn'] ?>">
+                                    <td>
+                                        <input type="checkbox" name="selection[]"  />
+                                    </td>
+
+                                    <td><?= $value['hn'] ?></td>
+                                    <td><div id="<?= 'v' . $value['pcc_vn'] ?>"></div></td>
+                                    <td><?= $value['visit_date_begin'] ?> <?= $value['visit_time_begin'] ?></td>                                    
+                                    <td><?= $value['fullname'] ?></td>
+                                    <td><span style='color:green'>Lab</span></td>
+                                </tr>                                
+                            <?php endforeach; ?>
+
 
 
                         </tbody>
@@ -76,5 +67,10 @@ use app\components\MessageHelper;
 
     </div>
 </div>
+<?php
+$js = <<< JS
+    $('#grid-view-data-table .table').DataTable();     
+JS;
+$this->registerJs($js);
 
 
