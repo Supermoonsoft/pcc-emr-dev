3b0fe52d96088509d0b86773c2d6496571e140e3
dhdc1 <dhdctraining@gmail.com>
2018-10-06 16:00:01 +0700

    auto

--- modules/report/views/default/next-dispen.php
+++ modules/report/views/default/next-dispen.php
@@ -11,47 +11,47 @@ use app\modules\stock\models\PccServiceMedication;
 DataTableAsset::register($this);
 ?>
 <div class="site-index">
-
+    
     <div class="panel panel-info">
         <div class="panel-heading">
-            <div class="panel-title"><i class="glyphicon glyphicon-list" aria-hidden="true"></i> ประมาณการการใช้ยาออกตรวจครั้งถัดไป</div>
+            <div class="panel-title"><i class="glyphicon glyphicon-list" aria-hidden="true"></i> ปริมาณการใช้ยาครั้งถัดไป</div>
         </div>
-        <div class="panel-body">            
-
-            <?php
-            $sql = " SELECT tmt24_code,drug_name,sum(qty) as total,unit
-                from pcc_service_medication
-                GROUP BY tmt24_code,drug_name,unit
-                ORDER BY drug_name ";
-            $raw = \Yii::$app->db->createCommand($sql)->queryAll();
-            $dataProvider = new ArrayDataProvider([
-                'allModels' => $raw,
-                'pagination' => FALSE
-            ]);
-
-            echo GridView::widget([
-                'id' => 'grid-view-data-table',
-                'dataProvider' => $dataProvider,
-                'layout' => '{items}',
-                'columns' => [
-                    [
-                        'class' => 'yii\grid\SerialColumn',
-                    ],
-                    'drug_name:text:รายการยา',
-                    'total:integer:รวมจำนวนจ่าย'
-                ],
-            ]);
-            ?>
-
+        <div class="panel-body"> 
+           
+                <table class="table  table-bordered">
+                    <thead>
+                        <tr>
+                            <th>#</th>                                
+                            <th>รายการ</th>
+                            <th >รวมจำนวนจ่าย</th>                           
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1; 
+                         $sql = " SELECT tmt24_code,drug_name,sum(qty) as total,unit
+                            from pcc_service_medication
+                            GROUP BY tmt24_code,drug_name,unit
+                            ORDER BY drug_name ";
+                        $raw = \Yii::$app->db->createCommand($sql)->queryAll();
+                        $dataProvider = new ArrayDataProvider([
+                            'allModels' => $raw,
+                            'pagination' => FALSE
+                        ]);
+                        ?>
+                        <?php foreach ($raw as $key => $data): ?>
+                            <tr>                         
+                                <td><?= $i++ ?></td>
+                                <td><?= $data['drug_name'] ?></td>                                
+                                <td><?= $data['total']?></td>                                
+                            </tr>                                
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
         </div>
     </div>
 
-
 </div>
-<?php
-$js = <<< JS
-    $('#grid-view-data-table .table').DataTable();     
-JS;
-$this->registerJs($js);
+
 
 
